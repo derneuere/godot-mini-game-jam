16ec56fe672143adafb81512565b020789b70c1b
Niaz <faridani.rad@gmail.com>
2023-10-28 16:09:42 +0200

    Add colisions back pls

--- GameOverText.cs
+++ GameOverText.cs
@@ -3,10 +3,10 @@ using System;
 
 public partial class GameOverText : Label
 {
-	public boolean shown
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		this.Visible = false;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
